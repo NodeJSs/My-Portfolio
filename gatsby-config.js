a6092e2435d8bb16093fd6a398b292b77471badc
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-04-12 21:22:44 +0100

    Edited theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,8 @@ module.exports = {
         name: `Faruq's Portfolio`,
         short_name: `Faruq's Portfolio`,
         start_url: `/`,
-        background_color: `#1d2229`,
-        theme_color: `#663399`,
+        background_color: `#663399`,
+        theme_color: `#1d2229`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
